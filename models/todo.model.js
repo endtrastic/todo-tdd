7e3270af8dd60c3f3e6e2773c8fea627c458622d
endtrastic <a.tuvikene@voco.ee>
2025-05-06 09:24:21 +0300

    Created TodoController.createTodo unit tests withdata and HTTP mocks

---
+++ models/todo.model.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const TodoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  done: {
+    type: Boolean,
+    default: false,
+  }
+});
+const TodoModel = mongoose.model('Todo', TodoSchema);
+module.exports = TodoModel;
